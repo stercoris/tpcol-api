ffde4e152286906c33c77d8cf46f9d0a599d48c0
stercoris <dimarodin0@gmail.com>
2021-03-04 18:58:31 +0400

    Убрал лишнее объявление

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,13 @@ import TPCRouter from "./tpcol";
 
 const server: Application = express();
 
-async function startup() {
+(async () => {
     // Запускаем TpcolASU API
     const options: OptionsData = {
         schema: TPCRouter,
         graphiql: true,
     };
     server.use("/tpcol-api", graphqlHTTP(options));
-}
-startup();
+})();
 
 server.listen(5000, () => console.log("server run"));
