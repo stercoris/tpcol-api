c8ae44c7bdc72574e0bf6ce43706ea6626b7e251
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-03-15 01:59:06 +0400

    exams page controller

---
+++ src/tpcol/exams_page.ts
@@ -0,0 +1,37 @@
+import Exam from "../entity/exam/Exam";
+import Page, { TPCPages } from "./page";
+
+export default class ExamsPage {
+    document: Document;
+
+    exams: Exam[];
+
+    public async init(group: number) {
+        const args = new URLSearchParams();
+        args.append("group", group.toString());
+        this.document = await Page.getPage(TPCPages.Exams, args);
+        this.exams = this.getExams();
+    }
+
+    private getExams(): Exam[] {
+        const exams: Exam[] = [];
+
+        const getColumn = (column: number) => `/html/body/table//tr[1]/td[2]/table[2]//tr[1]/td[2]/table//tr/td/table[2]//tr[position()>1]/td[${column}]/text()`;
+        const dates = Page.getByXPath(this.document, getColumn(1));
+        const professions = Page.getByXPath(this.document, getColumn(2));
+        const teachers = Page.getByXPath(this.document, getColumn(3));
+        const cabinets = Page.getByXPath(this.document, getColumn(4));
+
+        dates.forEach((date, i) => {
+            exams.push(
+                {
+                    date,
+                    cabinet: cabinets[i],
+                    profession: professions[i],
+                    teacher: teachers[i],
+                } as Exam,
+            );
+        });
+        return (exams);
+    }
+}
