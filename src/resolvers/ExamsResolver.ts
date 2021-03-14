31ede516df8de1efab2d4d5e255c2695a05bb512
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-03-15 01:59:25 +0400

    exams resolver

---
+++ src/resolvers/ExamsResolver.ts
@@ -0,0 +1,28 @@
+import {
+    Resolver,
+    Arg,
+    Query,
+    Int,
+    InputType,
+    Field,
+} from "type-graphql";
+import Exam from "../entity/exam/Exam";
+import { ExamsPage } from "../tpcol/exports";
+
+@InputType()
+class ExamsRequest {
+    @Field(() => Int)
+    group: number;
+}
+
+@Resolver()
+export default class ExamsResolver {
+    @Query(() => [Exam])
+    async Exams(
+        @Arg("params", () => ExamsRequest, { nullable: false }) request: ExamsRequest,
+    ) {
+        const examsPage = new ExamsPage();
+        await examsPage.init(request.group);
+        return (examsPage.exams);
+    }
+}
